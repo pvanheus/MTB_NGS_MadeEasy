48945c8fbac9f50948b1f1a87d1463bd0bcf1864
pvanheus <pvanheusden@uwc.ac.za>
2025-04-02 21:07:25 +0200

    Add information on continent and corresponding plot

--- plot_institute_types.R
+++ plot_institute_types.R
@@ -1,4 +1,4 @@
-pacman::p_load(ggplot2, tidyverse, rio, janitor)
+pacman::p_load(countrycode, dplyr, ggplot2, janitor, rio, tidyverse)
 
 active_participants <- import("SummaryParticipation - Sheet1.csv") %>%
   rename(title = `Title (e.g. Mr, Ms, Dr)`) %>%
@@ -31,6 +31,7 @@ active_participants <- import("SummaryParticipation - Sheet1.csv") %>%
     grepl("Instituto Adolfo Lutz", institution) ~ "public health",
     grepl("NCDC", institution) ~ "public health",
     grepl("Instituto de Salud Pública", institution) ~ "public health",
+    grepl("SRL Milan Network", institution) ~ "public health",  # Instituto de Diagnostico y Referencia Epidemiologicos
     grepl("Research Institute", institution, ignore.case = TRUE) ~ "research institute",
     grepl("MRC", institution) ~ "research institute",
     grepl("Institut Pasteur", institution) ~ "research institute",
@@ -52,17 +53,29 @@ active_participants <- import("SummaryParticipation - Sheet1.csv") %>%
     grepl("National Institute of  Health", institution) ~ "research institute",
     grepl("Guangzhou Institutes of Biomedicine and Health, Chinese Academy of Sciences, Guangzhou, China", institution) ~ "research institute",
     grepl("Centre Pasteur du Cameroun", institution) ~ "research institute",  # this serves both research and public health functions
-    grepl("Moh", institution) ~ "ministry",
-    grepl("NTP", institution) ~ "ministry",  # National TB Programme - is it Minister or Public Health institute?
-    grepl("National TB Control Program", institution) ~ "ministry",
+    grepl("Moh", institution) ~ "health ministry",
+    grepl("NTP", institution) ~ "health ministry",  # National TB Programme - is it Minister or Public Health institute?
+    grepl("National TB Control Program", institution) ~ "health ministry",
     grepl("Hamad Medical Corporation", institution) ~ "hospital",
     grepl("medical centre", institution, ignore.case = TRUE) ~ "hospital",
     grepl("Grande Ospedale Metropolitano Niguarda", institution) ~ "hospital",
     grepl("PATH", institution) ~ "nonprofit",
     grepl("MDH", institution) ~ "nonprofit",
-    grepl("WHO", institution) ~ "multilateral",
+    grepl("WHO", institution) ~ "WHO",
+    grepl("Elitechgroup S.p.A", institution) ~ "company",
     TRUE ~ "unknown"
   )) %>%
+  mutate(Country = replace(Country, Country == "Mocambique", "Mozambique")) %>%
+  mutate(Country = replace(Country, Country == "US", "USA")) %>%
+  mutate(Country = replace(Country, Country == "Philipines", "Philippines")) %>%
+  mutate(Country = replace(Country, Country == "?", NA)) %>%
+  mutate(Country = replace(Country, Country == "Cameroun", "Cameroon")) %>%
+  mutate(continent = countrycode(sourcevar = Country,
+                                 origin = "country.name", 
+                                 destination = "continent",
+                                 custom_match = c("Kosovo" = "Europe"))) %>%
+  mutate(continent = replace(continent, Country %in% c("Argentina", "Chile", "Brazil"), "South America")) %>%
+  mutate(continent = replace(continent, Country %in% c("Mexico", "USA", "Canada"), "North America")) %>%
   arrange(factor(Date, levels = c("March2022", 
                                   "January2023",
                                   "October 2023",
@@ -70,6 +83,38 @@ active_participants <- import("SummaryParticipation - Sheet1.csv") %>%
 
 unknown <- active_participants %>% filter(institution_type == "unknown") %>% select(institution, Country)
 
-by_date <- active_participants %>% group_by(Date, institution_type) %>% count()
+by_date <- active_participants %>% group_by(Date, institution_type) %>% count() %>%
+  arrange(factor(Date, levels = c("March2022", 
+                                  "January2023",
+                                  "October 2023",
+                                  "June2024")))
 
-ggplot(by_date, aes(x=Date, y=n, fill=institution_type)) + geom_bar(stat = "identity")
+by_country = active_participants %>% group_by(Date, Country) %>% count() %>%
+  arrange(factor(Date, levels = c("March2022", 
+                                  "January2023",
+                                  "October 2023",
+                                  "June2024")))
+
+by_continent = active_participants %>% group_by(Date, continent) %>% count() %>%
+  arrange(factor(Date, levels = c("March2022", 
+                                  "January2023",
+                                  "October 2023",
+                                  "June2024")))
+
+ggplot(by_date, aes(x = fct_inorder(Date), y = n, fill=institution_type)) +
+  geom_bar(stat = "identity") + 
+  labs(x = "Course", y = "Count",
+       fill = "Institution Type",
+       title = "Active Participants by Institution Type")
+  
+ggplot(by_country, aes(x = fct_inorder(Date), y = n, fill=Country)) +
+  geom_bar(stat = "identity") + 
+  labs(x = "Course", y = "Count",
+       fill = "Country",
+       title = "Active Participants by Country")
+
+ggplot(by_continent, aes(x = fct_inorder(Date), y = n, fill=continent)) +
+  geom_bar(stat = "identity") + 
+  labs(x = "Course", y = "Count",
+       fill = "Continent",
+       title = "Active Participants by Continent")
